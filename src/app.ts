729fe8557cbcb52ff01b78a2dd1decfcbaf6086a
`thais` <`tjlannes@gmail.com`>
2023-03-13 22:29:45 +0100

    modify app and test

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,18 @@
-export const hello = {
-  hello: 'Hello world',
-};
+import express from 'express';
+import apiRouter from './api/api-router.js';
+import cors from 'cors';
+
+const app = express();
+
+app.get('/', (_req, res) => {
+  res.json('Server ON');
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(cors({ origin: ['http://localhost:4000/'] }));
+app.use('/api/v1', apiRouter);
+
+app.disable('x-powered-by');
+
+export default app;
