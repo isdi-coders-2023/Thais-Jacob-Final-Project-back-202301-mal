5319c699d4f3c3de330cf0041668e92f5ad109d3
`thais` <`tjlannes@gmail.com`>
2023-03-14 23:35:19 +0100

    add dotenv, disable x powered and add log test

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,8 +1,8 @@
 import request from 'supertest';
-import app from './app.js';
+import app from './app';
 
 describe('Given an app', () => {
-  test('When the server is deployed, the app should respond with a message', async () => {
+  test('When the server starts, then the app should respond with a message', async () => {
     const res = await request(app).get('/');
     expect(res.body).toEqual('Server ON');
   });
--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,29 @@
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-import authRouter from './api/auth/auth-router.js';
-import errorHandler from './utils/error-handler.js';
 import apiRouter from './api/api-router.js';
+import authRouter from './api/auth/auth-router.js';
+import { errorHandler } from './utils/error-handler.js';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 
-app.disable('x-powered-by');
-
 app.use(cors());
-
 app.use(cors({ origin: ['http://localhost:4000/'] }));
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.json('Server ON');
 });
-
-app.use(express.json());
-
 app.use('/auth', authRouter);
 app.use('/api/v1', apiRouter);
 
 app.use(bodyParser.json());
 
 app.use(errorHandler);
+
+app.disable('x-powered-by');
 
 export default app;
