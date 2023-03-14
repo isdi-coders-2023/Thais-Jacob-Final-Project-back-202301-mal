4a40f3ccb82e4b33a835609cdac1daeebb76170b
`thais` <`tjlannes@gmail.com`>
2023-03-14 18:56:56 +0100

    add bodyParser and errorHandler in app

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,29 @@
 import express from 'express';
-import apiRouter from './api/api-router.js';
 import cors from 'cors';
+import bodyParser from 'body-parser';
+import authRouter from './api/auth/auth-router.js';
+import errorHandler from './utils/error-handler.js';
+import apiRouter from './api/api-router.js';
 
 const app = express();
 
-app.get('/', (_req, res) => {
+app.disable('x-powered-by');
+
+app.use(cors());
+
+app.use(cors({ origin: ['http://localhost:4000/'] }));
+
+app.get('/', (req, res) => {
   res.json('Server ON');
 });
 
-app.use(cors());
 app.use(express.json());
-app.use(cors({ origin: ['http://localhost:4000/'] }));
+
+app.use('/auth', authRouter);
 app.use('/api/v1', apiRouter);
 
-app.disable('x-powered-by');
+app.use(bodyParser.json());
+
+app.use(errorHandler);
 
 export default app;
