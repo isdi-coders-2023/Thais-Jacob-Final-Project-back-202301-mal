f5c5c263390363b828b55a40e550676f7d94e2df
`thais` <`tjlannes@gmail.com`>
2023-03-18 17:24:04 +0100

    add name validation

--- src/api/auth/auth-validation.ts
+++ src/api/auth/auth-validation.ts
@@ -2,6 +2,7 @@ import { Joi } from 'express-validation';
 
 export const authValidation = {
   body: Joi.object({
+    name: Joi.string(),
     email: Joi.string().email().required(),
     password: Joi.string()
       .regex(/[a-zA-Z0-9]{6,30}/)
