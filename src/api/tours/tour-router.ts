210b96dedc57a734911ef32f56a0b51069285cb6
`thais` <`tjlannes@gmail.com`>
2023-03-29 16:26:52 +0200

    add Joi validation in tour

--- src/api/tours/tour-router.ts
+++ src/api/tours/tour-router.ts
@@ -1,12 +1,16 @@
 import express from 'express';
+import { validate } from 'express-validation';
 import upload from '../../database/multer.js';
 import {
   createTourController,
   getTourByIdController,
   getToursController,
 } from './tour-controllers.js';
+import tourValidation from './tour-validation.js';
 
 const router = express.Router();
+
+router.use(validate(tourValidation));
 
 router.route('/').get(getToursController);
 router.route('/:_id').get(getTourByIdController);
